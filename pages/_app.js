910eee2367a144ae8ff9269dcb33dfe13fd830f5
praizoh <oremei.akande@gmail.com>
2022-08-15 18:08:57 +0100

    add appolo-client to query graphql

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
 
 const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID;
 
@@ -27,9 +29,11 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
+      <ApolloProvider client={client}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
+      </ApolloProvider>
       </RainbowKitProvider>
     </WagmiConfig>
   );
